f4498679fd94d20496c3d700eda0c732455d338d
filipe-freitas-px <filipe.freitas@px.center>
2026-05-18 10:26:24 -0300

    Update me_install.sh

--- me_install.sh
+++ me_install.sh
@@ -1,6 +1,15 @@
+#!/bin/bash
+
+# Instalação EC Agent
+TEMP=$(mktemp -d)
+cd $TEMP
 curl -fsSL https://github.com/filipe-freitas-px/ME/raw/refs/heads/main/PXCenter_UEMSLinuxAgent.zip -o ME_Install.zip
 unzip ME_Install.zip
 chmod +x UEMS_LinuxAgent.bin
 sudo ./UEMS_LinuxAgent.bin
+cd ../../ && rm -rf $TEMP
+who
+
+# TROCA DE HOSTNAME
 hostnamectl set-hostname "PX-$(sudo dmidecode -s system-serial-number)"
 echo "PX-$(sudo dmidecode -s system-serial-number)"
